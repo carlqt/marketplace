bdb6deede9b60813a1b4c9921dcb53153afd1fb0
Carl Tablante <carl.q.tablante@gmail.com>
2015-01-10 11:10:17 +0800

    added specs

--- spec/controllers/practice/enquiries_spec.rb
+++ spec/controllers/practice/enquiries_spec.rb
@@ -1,22 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Practice::EnquiriesController do
+  before(:each) do
+    @doctor_sign_in = proc {controller.stub(:load_practice).and_return(true)}
+  end
 
-  shared_examples_for 'all actions' do
-    it 'redirects to the home page when user is not a doctor'
+  shared_examples_for 'other actions' do |action, params|
+    it 'redirects to the home page when user is not a doctor' do
+      controller.instance_variable_set(:@practice, "nil")
+      get :"#{action}", params
+      expect(response).to redirect_to(root_path)
+    end
   end
 
   describe "#index" do
-    before(:each) do
-      @doctor = Doctor.new
-      @doctor.stub(:is_a_doctor?).and_return(true)
-      # controller.current_user
+    it "renders the index view" do
+      @doctor_sign_in.call
+      get :index
+      expect(response).to render_template :index
     end
 
-    it "renders the index view" do
-      get :index
-      binding.pry
-      response.should render_template :index
+    it_behaves_like 'other actions', 'index'
+  end
+
+  describe "#show" do
+    it "renders the show view" do
+      @doctor_sign_in.call
+      doctor = Enquiry.create!
+      get :show, {id: 1}
+      expect(response).to render_template(:show)
+    end
+
+    it_behaves_like 'other actions', 'show', id: 1
+  end
+
+  describe "#confirm" do
+    it "redirects to enquiry if confirmed" do
+      @doctor_sign_in.call
+      Enquiry.create
+      put :confirm, id: 1, enquiry: { status: "confirm" }
+      expect(response).to redirect_to(practice_enquiries_path(1))
     end
   end
+
+  describe "#reject" do
+    it "redirects to enquiry if confirmed" do
+      @doctor_sign_in.call
+      Enquiry.create
+      put :reject, id: 1, enquiry: { status: "reject" }
+      expect(response).to redirect_to(practice_enquiries_path(1))
+    end
+  end
+
 end
